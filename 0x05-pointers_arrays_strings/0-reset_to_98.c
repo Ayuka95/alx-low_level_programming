1728e39ab3e1424a628affd4038a56c0adfad620
Ayuka95 <hayatantheneh@gmail.com>
2022-07-13 11:30:07 +0300

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,29 +1,13 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
- * @src: second string to copy from
- * @dest: string to be overwritten
- * Description: concatenates src to end of dest
- * Return: pointer to dest
+ * reset_to_98 - set value of int to 98
+ * @n: address of value to change
+ * Description: resets *n to 98
+ * Return: nothing
  **/
 
-char *_strcat(char *dest, char *src)
+void reset_to_98(int *n)
 {
-	int i, j;
-
-	i = 0;
-
-	while (dest[i] != '\0')
-		i++;
-
-	for (j = 0; src[j] != '\0'; j++)
-	{
-		dest[i] = src[j];
-		i++;
-	}
-
-	dest[i] = src[j];
-
-	return (dest);
+	*n = 98;
 }
