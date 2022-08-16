bc2ac3b83f2c51620d54a83f722756b29a744db9
Ayuka95 <hayatantheneh@gmail.com>
2022-08-16 12:02:04 +0300

    12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -38,4 +38,25 @@ size_t print_listint_safe(const listint_t *head)
 	{
 		new = malloc(sizeof(listp_t));
 		if (new == NULL)
-
+			exit(98);
+		new->p = (void *)head;
+		new->next = hptr;
+		hptr = new;
+		add = hptr;
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&hptr);
+				return (nodes);
+			}
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nodes++;
+	}
+	free_listp(&hptr);
+	return (nodes);
+}
