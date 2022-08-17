6b1b9249ead82869ad96938df3e8a0059b898e10
Ayuka95 <hayatantheneh@gmail.com>
2022-08-17 11:34:05 +0300

    3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -41,4 +41,29 @@ int op_mul(int a, int b);
  * Return: a / b, or error if b = 0
  */
 int op_div(int a, int b);
+/**
+ * op_mod - modulo a to b
+ *
+ * @a: num1
+ * @b: num2
+ *
+ * Return: a % b
+ */
+int op_mod(int a, int b);
 
+
+int (*get_op_func(char *s))(int, int);
+
+/**
+ * struct op - Struct op
+ *
+ * @op: the operator
+ * @f: the function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
+#endif
